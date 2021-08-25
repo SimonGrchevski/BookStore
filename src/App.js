1858a0268fca1203561b82f3f5751922dce22b6f
SimonGrchevski <simongrcevski@gmail.com>
2021-08-25 13:41:35 +0200

    Add a Categories section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import Categories from './components/Categories';
 import Header from './components/Header';
 import Library from './components/Library';
+import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return(
     <div>
       <Header />
-      <Library />
+      <Switch>
+        <Route path='/'>
+          <Library />
+        </Route>
+        <Route path='/Categories'>
+          <Categories />
+        </Route>
+      </Switch>
     </div>
   );
 }
