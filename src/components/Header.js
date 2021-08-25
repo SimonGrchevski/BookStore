e5373b359dc071504376660d0656ff669300cdaa
SimonGrchevski <simongrcevski@gmail.com>
2021-08-25 11:32:05 +0200

    Add the header component and install react-router-dom

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>BookStore</h1>
+      <ul>
+        <li>Books</li>
+        <li>Categories</li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
