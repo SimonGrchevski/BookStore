43bb6d2aec8667922635d09e72c128d2f3ea18ce
SimonGrchevski <simongrcevski@gmail.com>
2021-08-27 19:15:45 +0200

    Refactor the ugly algorithm

--- src/components/utility.js
+++ src/components/utility.js
@@ -3,26 +3,19 @@ import Book from './Book';
 
 const generateBooks = () => {
   const state = useSelector((state) => state);
-  const values = Object.values(state.books);
-  const ids = Object.keys(state.books);
-  let idCounter = -1; // for the 0 start at values.map
-
-  const books = values.map((e) => {
-    idCounter += 1;
-    return (
-      <li key={ids[idCounter]}>
-        <Book
-          title={e[0].title}
-          author={e[0].author || 'Author'}
-          category={e[0].category}
-          completed={32}
-          chapter="chapter"
-          id={ids[idCounter]}
-          key={ids[idCounter]}
-        />
-      </li>
-    );
-  });
+  const books = Object.entries(state.books).map(([k, v]) => (
+    <li key={k}>
+      <Book
+        title={v[0].title}
+        author={v[0].author || 'Author'}
+        category={v[0].category}
+        completed={32}
+        chapter="chapter"
+        id={k}
+        key={k}
+      />
+    </li>
+  ));
   return books;
 };
 
